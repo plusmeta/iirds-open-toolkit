7055a7a62d807bfd7db409a26612e02664461333
Jan Oevermann <jan@plusmeta.de>
2020-05-16 17:36:24 +0200

    Repo-Anpasungen

--- script/i18n.report.js
+++ script/i18n.report.js
@@ -9,7 +9,6 @@ function reportMissingKeys () {
     const extractType = 1; // EXTRACT_MISSING = 1; EXTRACT_UNUSED = 2; EXTRACT_ALL = 3;
 
     const i18nReport = i18nExtract.createI18NReport(srcFiles, localeFiles, extractType);
-    i18nExtract.logI18NReport(i18nReport);
     if (i18nReport.missingKeys.length) {
         console.log(chalk.red("Missing keys detected!\n"));
         process.exit(1);
