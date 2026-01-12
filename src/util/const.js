2766a74fa87d43ee66b2f707edf9fdd604260556
Gerhard Glatz <gerhard@plusmeta.de>
2026-01-12 08:40:34 +0100

    feat: add const

---
+++ src/util/const.js
@@ -0,0 +1,19 @@
+export class IdConst {
+    static IIRDS_VARIANT_A = "A";
+    static IIRDS_VARIANT_H = "H";
+    static IIRDS_VARIANT_UNRESTRICTED = "unrestricted";
+    static IIRDS_PARTYROLE_MANUFACTURER = "http://iirds.tekom.de/iirds#Manufacturer";
+    static IIRDS_PARTYROLE_AUTHOR = "http://iirds.tekom.de/iirds#Author"
+    static IIRDS_PARTYROLE_CREATOR = "http://iirds.tekom.de/iirds#Creator";
+    static IIRDS_OBJECTINSTANCEURI = "http://iirds.tekom.de/iirds#ObjectInstanceURI";
+    static IIRDS_OBJECTTYPEURI= "http://iirds.tekom.de/iirds#ObjectTypeURI";
+    static IIRDS_SERIALNUMBER= "http://iirds.tekom.de/iirds#SerialNumber";
+    static IIRDS_PRODUCTTYPE = "http://iirds.tekom.de/iirds#ProductType";
+    static PLUS_IIRDSVARIANT = "plus:iirdsVariant";
+}
+
+export class FileConst {
+    static METADATA_RDF = "META-INF/metadata.rdf";
+    static METADATA_JSONLD = "META-INF/metadata.jsonld";
+}
+
